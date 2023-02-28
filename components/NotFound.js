470a0d4f3d8ac9f4e4f51471554e5c79e44bac56
vithun5 <rvithun99@gmail.com>
2023-02-28 19:25:15 +0530

    notFoundPage Updated

--- components/NotFound.js
+++ components/NotFound.js
@@ -1,5 +1,33 @@
+import Image from "next/image";
+import { useContext } from "react";
+import { SearchContext } from "../layout/Layout";
+import { NotFoundContainer } from "../styles/Home.styled";
+import TitleDesc from "./TitleDesc";
 const NotFound = () => {
-  return <h1>notfound</h1>;
+  const { handleClickHere } = useContext(SearchContext);
+
+  return (
+    <div>
+      <TitleDesc title="Not Found" desc="Oops, No result found!" />
+      <NotFoundContainer>
+        <div>
+          <Image
+            src="/assets/notResultFound.png"
+            alt="no search found"
+            width={300}
+            height={250}
+            style={{
+              zIndex: "-1",
+            }}
+          />
+          <p>
+            {`Please search as "Vithun Shankar" or `}
+            <a onClick={handleClickHere}>Click here.</a>
+          </p>
+        </div>
+      </NotFoundContainer>
+    </div>
+  );
 };
 
 export default NotFound;
